6005911af0aebb8f027ecf2c4c9e21eb64253f3a
ZARIFMAC <uzbzarif@gmail.com>
2022-09-24 14:47:36 +0500

    result

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,33 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Text('Simple Currency converter')),
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          children: [
+            const Padding(
+              padding: const EdgeInsets.only(top: 150),
+              child: const Text(
+                'Result:',
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Color.fromARGB(100, 174, 174, 174),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: const Text(
+                '\$100',
+                style: TextStyle(
+                  fontSize: 64,
+                  color: Color.fromARGB(100, 107, 107, 107),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
