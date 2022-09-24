e6cc3da6185a034d3b169d06f84c87ca32309c7a
ZARIFMAC <uzbzarif@gmail.com>
2022-09-24 15:43:52 +0500

    ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -83,6 +83,14 @@ class _MyAppState extends State<MyApp> {
                   ),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('CONVERT'),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size.fromHeight(50))),
             )
           ],
         ),
