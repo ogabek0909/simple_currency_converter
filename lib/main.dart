6e5d7098d8565691a92f8f97a2d07f0ef897c835
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-09-27 05:18:15 +0500

    changer

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,36 @@ class MyApp extends StatefulWidget {
 TextEditingController text1 = TextEditingController(text: '');
 
 class _MyAppState extends State<MyApp> {
-  String name = '0';
+  String name = '\$0';
+  String b = '';
+  String us = '🇺🇸';
+  String c = '🇺🇸';
+  String uzb = '🇺🇿';
   double a = 0;
 
-  func() {
-    a = double.parse(text1.text) * 0.00011085;
+  changer() {
+    //uzb = '';
+    //us = '';
     setState(() {
-      name = '\$${a.toStringAsPrecision(2)}';
+      us = uzb;
+      uzb = c;
+      c = us;
+    });
+  }
+
+  func() {
+    a = double.parse(text1.text);
+    if (uzb == '🇺🇸') {
+      a = a * 11000;
+      b = '${a}';
+      name = '0';
+    } else {
+      a = a * 0.00011;
+      b = '\$${a.toStringAsPrecision(2)}';
+    }
+    //a = double.parse(text1.text) * 0.00011;
+    setState(() {
+      name = b;
     });
   }
 
@@ -30,89 +53,95 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     //String name=text1.text;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 150),
-              child: Text(
-                'Result:',
-                style: TextStyle(
-                  fontSize: 34,
-                  color: Color.fromARGB(100, 174, 174, 174),
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: Text(
+                  'Result:',
+                  style: TextStyle(
+                    fontSize: 34,
+                    color: Color.fromARGB(100, 174, 174, 174),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15),
-              child: Text(
-                name,
-                style: TextStyle(
-                  fontSize: 64,
-                  color: Color.fromARGB(100, 107, 107, 107),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 64,
+                    color: Color.fromARGB(100, 107, 107, 107),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
-              child: TextField(
-                controller: text1,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    label: Text('Amount'),
-                    border: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromARGB(100, 107, 107, 107)))),
+              Padding(
+                padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+                child: TextField(
+                  controller: text1,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      label: Text('Amount'),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(100, 107, 107, 107)))),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('From'),
-                      Text(
-                        '🇺🇿',
-                        style: TextStyle(fontSize: 60),
-                      ),
-                    ],
-                  ),
-                  Icon(
-                    Icons.sync_alt,
-                    size: 60,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('To'),
-                      Text(
-                        '🇺🇸',
-                        style: TextStyle(fontSize: 60),
-                      ),
-                    ],
-                  ),
-                ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('From'),
+                        Text(
+                          uzb,
+                          style: TextStyle(fontSize: 60),
+                        ),
+                      ],
+                    ),
+                    OutlinedButton(
+                        onPressed: changer,
+                        child: Icon(
+                          Icons.sync_alt,
+                          size: 60,
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('To'),
+                        Text(
+                          us,
+                          style: TextStyle(fontSize: 60),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ElevatedButton(
-                  onPressed: func,
-                  child: Text(
-                    'CONVERT',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      minimumSize: Size.fromHeight(60))),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                    onPressed: func,
+                    child: Text(
+                      'CONVERT',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: Size.fromHeight(60))),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
